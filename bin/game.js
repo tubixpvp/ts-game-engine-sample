4aefcef677754e9994592aa0a4f96dd77dfe2b62
Tubix <vdmitriev556@gmail.com>
2023-10-22 10:46:31 +0100

    update

--- bin/game.js
+++ bin/game.js
@@ -5,17 +5,28 @@ var main;
     var Camera3D = gameengine.engine.components.Camera3D;
     var MeshLoader = gameengine.engine.resources.MeshLoader;
     var MeshRenderer = gameengine.engine.components.MeshRenderer;
+    var TextureMaterial = gameengine.engine.material.TextureMaterial;
+    var Transform3D = gameengine.engine.components.Transform3D;
     class Main {
         constructor() {
             const scene = new GameScene();
             const cameraObject = new GameObject();
-            const camera = cameraObject.addComponent(Camera3D);
+            cameraObject.addComponent(Camera3D);
+            const cameraTransform = cameraObject.getComponent(Transform3D);
+            cameraTransform.position.z -= 1000;
+            cameraTransform.getObject3D().lookAt(0, 0, 0);
             scene.addChild(cameraObject);
             scene.start();
-            MeshLoader.loadMesh("3ds", "resources/object.3ds", (meshObject) => {
-                let mesh = meshObject.getComponent(MeshRenderer);
-                console.log(mesh);
-                scene.addChild(meshObject);
+            MeshLoader.loadMesh("fbx", "resources/object.fbx", (meshObject) => {
+                const mesh = meshObject.getComponent(MeshRenderer);
+                const texture = new Image();
+                texture.onload = () => {
+                    mesh.material = new TextureMaterial(TextureMaterial.MESH_BASIC, texture);
+                    scene.addChild(meshObject);
+                    meshObject.getComponent(Transform3D).position.set(0, 0, 0);
+                    console.log(mesh);
+                };
+                texture.src = "resources/texture.jpg";
             });
         }
     }
--- src/Main.ts
+++ src/Main.ts
@@ -5,23 +5,38 @@ module main {
     import Camera3D = gameengine.engine.components.Camera3D;
     import MeshLoader = gameengine.engine.resources.MeshLoader;
     import MeshRenderer = gameengine.engine.components.MeshRenderer;
+    import TextureMaterial = gameengine.engine.material.TextureMaterial;
+    import Transform3D = gameengine.engine.components.Transform3D;
     export class Main {
         public constructor() {
             const scene: GameScene = new GameScene();
 
             const cameraObject: IGameObject = new GameObject();
-            const camera: Camera3D = cameraObject.addComponent(Camera3D);
+            cameraObject.addComponent(Camera3D);
+
+            const cameraTransform:Transform3D = cameraObject.getComponent(Transform3D);
+            cameraTransform.position.z -= 1000;
+            cameraTransform.getObject3D().lookAt(0,0,0);
 
             scene.addChild(cameraObject);
 
             scene.start();
 
-            MeshLoader.loadMesh("3ds", "resources/object.3ds", (meshObject:IGameObject):void =>
+            MeshLoader.loadMesh("fbx", "resources/object.fbx", (meshObject:IGameObject):void =>
             {
-                let mesh:MeshRenderer = meshObject.getComponent(MeshRenderer);
-                console.log(mesh);
+                const mesh:MeshRenderer = meshObject.getComponent(MeshRenderer);
 
-                scene.addChild(meshObject);
+                const texture:HTMLImageElement = new Image();
+                texture.onload = ()=>{
+                    mesh.material = new TextureMaterial(TextureMaterial.MESH_BASIC,texture);
+
+                    scene.addChild(meshObject);
+
+                    meshObject.getComponent(Transform3D).position.set(0,0,0);
+
+                    console.log(mesh);
+                };
+                texture.src = "resources/texture.jpg";
             });
         }
     }
